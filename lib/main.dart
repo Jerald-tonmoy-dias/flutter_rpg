e56092704be01b31b7fe53f587d90e4e8db8c441
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2025-07-05 22:02:48 +0600

    initial setup

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/screens/home/home.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Home()));
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sandbox'), centerTitle: true),
+      backgroundColor: Colors.grey,
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: const Text("Sandbox"),
+      ),
+    );
+  }
+}
