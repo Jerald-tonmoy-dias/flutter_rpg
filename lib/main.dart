857878c08b85830747b876c368bf009894609c51
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2025-07-23 23:13:53 +0600

    set global store Proved & and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rpg/screens/create/create.dart';
 import 'package:flutter_rpg/screens/home/home.dart';
+import 'package:flutter_rpg/services/character_store.dart';
 import 'package:flutter_rpg/theme.dart';
+import 'package:provider/provider.dart';
+// firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: primaryTheme,
-      home: Home(),
-      // home: Create(),
+    ChangeNotifierProvider(
+      create: (context) => CharacterStore(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: primaryTheme,
+        home: Home(),
+      ),
     ),
   );
 }
